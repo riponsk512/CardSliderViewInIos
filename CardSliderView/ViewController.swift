5f30ff7bb9f96083b7c2f0defb539fd1288b85e7
Ripon Sk <riponsk477@gmail.com>
2022-10-07 19:43:32 +0530

    Initial Commit

---
+++ CardSliderView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardSliderView
+//
+//  Created by Ripon sk on 07/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
